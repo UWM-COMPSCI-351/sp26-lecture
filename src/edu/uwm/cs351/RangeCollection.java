049dff94e7479cdb772e7e527604f228b266c89b
boyland <boyland@UWM014851.ad.uwm.edu>
2026-02-12 09:47:34 -0600

    fixed compiler errors.

---
+++ src/edu/uwm/cs351/RangeCollection.java
@@ -0,0 +1,24 @@
+package edu.uwm.cs351;
+
+import java.util.AbstractCollection;
+import java.util.Iterator;
+
+public class RangeCollection extends AbstractCollection<Integer> {
+
+	public RangeCollection(int lo, int hi) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Iterator<Integer> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
